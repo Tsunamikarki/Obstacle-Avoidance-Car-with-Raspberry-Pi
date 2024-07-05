fb385ab23687663dc3568164c9b4d81f4526fec3
Tsunami Karki <karkeesunamibns55@gmail.com>
2024-07-05 15:01:38 -0400

    Refined logic

--- obstacleAvoidance.py
+++ obstacleAvoidance.py
@@ -6,8 +6,8 @@ enRt=33 #enA
 enLt=35 #enB
 rtFwd=11 #in1
 rtRev=13 #in2
-ltFwd=18 #in3
-ltRev=16 #in4 
+ltFwd=16 #in3
+ltRev=18 #in4 
 
 #ultrasonic sensor
 trig=29
@@ -36,7 +36,7 @@ GPIO.setup(echo, GPIO.IN)
 
 #servo motor
 GPIO.setup(servoPin, GPIO.OUT)
-servo=GPIO.PWM(servoPin, 50) #50 is 50Hz pulse
+servo=GPIO.PWM(servoPin, 48) #50 is 50Hz pulse
 servo.start(0)
 
 def getDistance():
@@ -68,10 +68,12 @@ def rotateServo():
         servoDistance=getDistance()
         disAtScan[i]=servoDistance
         print(f"Distance at angle {angles[i]}: {round(servoDistance, 2)} cm ")
-        
+
 #start PWM
-speedEnRt.start(50)
+speedEnRt.start(50) 
 speedEnLt.start(50)
+
+setServoAngle(90)  
 
 #Forward Motion 
 def forward(): 
@@ -79,6 +81,7 @@ def forward():
 	GPIO.output(rtRev, GPIO.LOW)
 	GPIO.output(ltFwd, GPIO.HIGH)
 	GPIO.output(ltRev, GPIO.LOW)
+	print("Moving forward")
 
 #Backward Motion
 def reverse():
@@ -108,27 +111,32 @@ def stop():
 	GPIO.output(ltFwd, GPIO.LOW)
 	GPIO.output(ltRev, GPIO.LOW)
 
-while True:              
-    stop()
-    rotateServo()                            
+while True:                      
     frontDis=disAtScan[1]
     rightDis=disAtScan[0]
     leftDis=disAtScan[2]
     if(frontDis < 20):
         print("Obstacle Detected")
-		#stop()
-		#rotateServo()
-		#setServoAngle(90) 
+        rotateServo()
+        setServoAngle(90)
+        time.sleep(0.2)
         if (rightDis > safeDis and rightDis > leftDis):
             right()
             print("Turning right")
-            time.sleep(1)
+            time.sleep(0.4)
         elif(leftDis > safeDis and leftDis > rightDis):
             left()
             print("Turning left")     
-            time.sleep(1)
+            time.sleep(0.4)
+        elif(leftDis > safeDis and rightDis > safeDis):
+            if(leftDis > rightDis):
+                left()
+                time.sleep(0.4)
+            else:
+                right()
+                time.sleep(0.4)
     else:
         forward()
-        print("No obstacle. Moving forward!")     
-        
+        print("No obstacle. Moving forward!") 
+
 GPIO.cleanup()
